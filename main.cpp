c74fa78aeb51acd7be6fb685b7a0e8ef80c06f39
Belal Hamdy <belalhezzat@gmail.com>
2020-11-13 19:10:52 +0200

    Implemented finding solution using genetic algorithm approach with elitist and steady-state replacements

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
 #include <iostream>
 #include <vector>
 #include <random>
@@ -14,52 +17,46 @@ mt19937 g_RNG(chrono::steady_clock::now().time_since_epoch().count());
  * crossover (Done)
  * mutate (Done)
  */
-struct GeneticAlgorithm {
-    struct Chromosome {
-        vector<int> data;
-        int maxValue;
+class Chromosome {
+public:
+    vector<int> data;
+    int maxValue;
 
-        Chromosome(int nSize, int maxValue) : maxValue(maxValue), data(nSize) {
-            uniform_int_distribution<int> uid(0, maxValue);
-            for (int &v : data)
-                v = uid(g_RNG);
+    Chromosome(){}
+    Chromosome(int nSize, int maxValue) : maxValue(maxValue), data(nSize) {
+        uniform_int_distribution<int> uid(0, maxValue);
+        for (int &v : data)
+            v = uid(g_RNG);
+    }
+
+    void mutate() {
+        uniform_int_distribution<int> mutateIdx(0, data.size() - 1);
+        uniform_int_distribution<int> mutateValue(0, maxValue);
+        data[mutateIdx(g_RNG)] = mutateValue(g_RNG);
+    }
+
+    [[nodiscard]] pair<Chromosome, Chromosome> combineWith(const Chromosome &other) const {
+        Chromosome ret1 = *this;
+        Chromosome ret2 = other;
+        uniform_int_distribution<int> mutateOffset(0, data.size());
+        int off = mutateOffset(g_RNG);
+        for (int i = 0; i < off; ++i) {
+            ret1.data[i] = other.data[i];
+            ret2.data[i] = this->data[i];
         }
+        return {ret1, ret2};
+    }
 
-        void mutate() {
-            uniform_int_distribution<int> mutateIdx(0, data.size() - 1);
-            uniform_int_distribution<int> mutateValue(0, maxValue);
-            data[mutateIdx(g_RNG)] = mutateValue(g_RNG);
-        }
+};
 
-        pair<Chromosome, Chromosome> combineWith(const Chromosome &other) const {
-            Chromosome ret1 = *this;
-            Chromosome ret2 = other;
-            uniform_int_distribution<int> mutateOffset(0, data.size());
-            int off = mutateOffset(g_RNG);
-            for (int i = 0; i < off; ++i) {
-                ret1.data[i] = other.data[i];
-                ret2.data[i] = this->data[i];
-            }
-            return {ret1, ret2};
-        }
-
-    };
-
+class GeneticAlgorithm {
     const vector<int> &weaponInstances;
     const vector<vector<double>> &success;
     const vector<int> &threat;
-    vector<pair<Chromosome, double>> population;
+    vector<Chromosome> population;
 
-    GeneticAlgorithm(const vector<int> &wi,
-                     const vector<int> &th,
-                     const vector<vector<double>> &su, int populationSize)
-            : weaponInstances(wi), threat(th), success(su) {
-
-        for (int i = 0; i < populationSize; ++i) {
-            Chromosome curr(wi.size(), su[0].size());
-            population.emplace_back(curr, getFitness(curr));
-        }
-    }
+    Chromosome bestChromosome;
+    double bestFitness = INT_MAX;
 
     double getFitness(const Chromosome &ch) {
         vector<double> survive;
@@ -76,17 +73,84 @@ struct GeneticAlgorithm {
         return ans;
     }
 
+    vector<double> getChromosomesFitness() {
+        vector<double> fitness;
+        for (auto &i : population) fitness.push_back(getFitness(i));
+        return fitness;
+    }
+
     void nextGeneration() {
-        double totalFitness = 0.0;
+        // Construct probability for every chromosome
+        vector<double> chromosomesFitness = getChromosomesFitness();
         vector<double> probs;
-        for (auto &i : population) totalFitness += i.second;
-        for (auto &i : population) probs.push_back(i.second /= totalFitness);
+        double totalFitness = 0.0;
 
+        for (auto &i : chromosomesFitness) totalFitness += i;
+        probs.reserve(chromosomesFitness.size());
+        for (auto &i : chromosomesFitness) probs.push_back(i /= totalFitness);
+
+        // Roulette wheel construction
         discrete_distribution<> rouletteWheel(probs.begin(), probs.end());
-        Chromosome firstParent = population[rouletteWheel(g_RNG)].first;
-        Chromosome secondParent = population[rouletteWheel(g_RNG)].first;
 
-        // TODO: Here you have 2 parents mutate them and generate the next generation and fill it in the population
+        // select 2 parents
+        int firstParentIdx = rouletteWheel(g_RNG), secondParentIdx = rouletteWheel(g_RNG);
+        Chromosome firstParent = population[0];
+        Chromosome secondParent = population[1];
+
+        // get the children
+        pair<Chromosome, Chromosome> offSprings = firstParent.combineWith(secondParent);
+
+        // remove the parents from the population
+        population.erase(population.begin() + max(firstParentIdx, secondParentIdx));
+        population.erase(population.begin() + min(firstParentIdx, secondParentIdx));
+
+        // mutate children
+        offSprings.first.mutate();
+        offSprings.second.mutate();
+
+        // add children to population
+        population.push_back(offSprings.first);
+        population.push_back(offSprings.second);
+
+        // check if any of the children fitness is the current best in the population
+        double firstFitness = getFitness(offSprings.first);
+        double secondFitness = getFitness(offSprings.second);
+
+        if (firstFitness < bestFitness) {
+            bestFitness = firstFitness;
+            bestChromosome = offSprings.first;
+        }
+        if (secondFitness < bestFitness) {
+            bestFitness = secondFitness;
+            bestChromosome = offSprings.second;
+        }
+
+    }
+
+public:
+    GeneticAlgorithm(const vector<int> &wi,
+                     const vector<int> &th,
+                     const vector<vector<double>> &su, int populationSize)
+            : weaponInstances(wi), threat(th), success(su) {
+        for (int i = 0; i < populationSize; ++i) {
+            Chromosome curr(wi.size(), su[0].size());
+            population.push_back(curr);
+        }
+
+        for (const auto & i : population) {
+            double currFitness = getFitness(i);
+            if (currFitness < bestFitness) {
+                bestChromosome = i;
+                bestFitness = currFitness;
+            }
+        }
+    }
+
+    pair<Chromosome, double> execute(int epochs) {
+        while (epochs--) {
+            nextGeneration();
+        }
+        return {bestChromosome, bestFitness};
     }
 
 
@@ -95,6 +159,7 @@ struct GeneticAlgorithm {
 int main() {
     cout << "Enter the weapon types and the number of instances of each type: (Enter 'x' when you're done)" << endl;
     vector<string> weapons;         //nWeapons
+    vector<int> weaponCount;
     vector<int> weaponInstances;    //nWeaponInstances
 
     while (true) {
@@ -104,6 +169,7 @@ int main() {
         if (name == "x") break;
         weapons.push_back(name);
         cin >> count;
+        weaponCount.push_back(count);
         while (count--)
             weaponInstances.push_back(weapons.size() - 1);
     }
@@ -126,16 +192,36 @@ int main() {
     cout << "Please wait while running the GA..." << endl;
 
     //Do processing here
+    int populationSize = 20;
+    GeneticAlgorithm algorithmModel(weaponInstances, threat, success, populationSize);
+
+    int epochs = 1000;
+    pair<Chromosome, double> solution = algorithmModel.execute(epochs);
+    // chromosome solution is nWeaponInstances size
 
     cout << "The final WTA solution is:" << endl;
-    /*
-    Tank #1 is assigned to target #1,
-    Tank #2 is assigned to target #3,
-    Aircraft #1 is assigned to target #1,
-    Grenade #1 is assigned to target #2,
-    Grenade #2 is assigned to target #2,
-    */
-    double expectedThreat = 0;
-    cout << "The expected total threat of the surviving targets is " << expectedThreat << endl;
+    vector<int> solutionData = solution.first.data;
+    vector<int> weaponCountTemp(weaponCount);
+
+    for (int i = 0; i < solutionData.size(); ++i) {
+        int currWeaponInstance = weaponInstances[i];
+        cout << weapons[currWeaponInstance] << " " << "#" <<
+        weaponCount[currWeaponInstance] - --weaponCountTemp[currWeaponInstance] <<
+        " is assigned to target #" << solutionData[i] << ",\n";
+    }
+    cout << "The expected total threat of the surviving targets is " << solution.second << endl;
+
+
+    return 0;
 
 }
+/* Best Solution Generated by this code
+The final WTA solution is:
+Tank #1 is assigned to target #2,
+Tank #2 is assigned to target #0,
+Aircraft #1 is assigned to target #0,
+Grenade #1 is assigned to target #0,
+Grenade #2 is assigned to target #1,
+The expected total threat of the surviving targets is 15.048
+ */
+
